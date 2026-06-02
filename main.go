03c2520197f77d4fdc4543067fbf3501a8d0afd5
ashy558 <266838684+ashy558@users.noreply.github.com>
2026-06-02 11:22:51 +0100

    add help and exit commands

--- main.go
+++ main.go
@@ -6,17 +6,58 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	defer os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex`)
+	return nil
+}
+
 func main() {
+	supportedCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
 		if err := scanner.Err(); err != nil {
-			fmt.Printf(`an error occurred while scanning: %v`, err)
+			fmt.Printf(`scanner error: %v`, err)
 			break
 		}
 		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		command := cleanedInput[0]
+		cmd, ok := supportedCommands[command]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := cmd.callback(); err != nil {
+			fmt.Printf("%s: callback error: %v\n", cmd.name, err)
+		}
 	}
 }
