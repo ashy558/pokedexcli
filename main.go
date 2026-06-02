d66982e0ac64e0f03097041129155bd045139906
ashy558 <266838684+ashy558@users.noreply.github.com>
2026-06-02 10:42:14 +0100

    add repl pokedex loop

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		if err := scanner.Err(); err != nil {
+			fmt.Printf(`an error occurred while scanning: %v`, err)
+			break
+		}
+		cleanedInput := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleanedInput[0])
+	}
 }
